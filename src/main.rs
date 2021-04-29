7f0e1cfd44b5d89f9723e7eac3b6def0f7654f29
Steven Tzeng <steven.tzeng@gmail.com>
2021-04-29 18:40:50 +0800

    gen random number and return order by user input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     print!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -9,6 +16,14 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    print!("You guess: {}", guess);
 
+    let guess:u32 = guess.trim().parse().expect("Please type a number");
+
+    println!("You guess: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Too big!")
+    }
 }
