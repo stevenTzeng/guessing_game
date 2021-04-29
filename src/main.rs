d18ce747004806e305eac3c8426339c8fe33270e
Steven Tzeng <steven.tzeng@gmail.com>
2021-04-29 17:47:17 +0800

    handle user input from terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Guess the number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    print!("You guess: {}", guess);
+
 }
